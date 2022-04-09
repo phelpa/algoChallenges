ffade296c09623292de8c83c4db940bad48149b0
phelpa <pliniohelpa@gmail.com>
2022-04-09 19:43:10 -0300

    first commit, first challenge

---
+++ groupBySellers/index.js
@@ -0,0 +1,8 @@
+import { sales } from './sales.js'
+
+//Group the array by sellers and order by their total revenue
+//Seller with the highest revenue should be shown at the top with position 1
+//All the revenue values should only consider Confirmed orders
+// All dollar amounts should be displayed to 2 decimal places
+
+console.log(sales)
